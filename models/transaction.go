c459ebacbd9ef5c870a35076bda5e0d7c796c1fb
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2024-01-26 15:55:27 -0300

    convert bytea fields into string

--- models/transaction.go
+++ models/transaction.go
@@ -3,9 +3,9 @@ package models
 type Transaction struct {
 	BlockNum uint64  `json:"block_num"`
 	Value    float64 `json:"value"`
-	From     []byte  `json:"from"`
-	To       []byte  `json:"to"`
-	TxHash   []byte  `json:"tx_hash"`
+	From     string  `json:"from"`
+	To       string  `json:"to"`
+	TxHash   string  `json:"tx_hash"`
 }
 
 func (t Transaction) Type() ModelType {
