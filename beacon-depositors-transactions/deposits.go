cedd52815fabfd3ecdcb38e059134a476129dfcd
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2024-01-30 09:38:36 -0300

    implement multithreading and add internal category to request

--- beacon-depositors-transactions/deposits.go
+++ beacon-depositors-transactions/deposits.go
@@ -1,14 +1,103 @@
 package beacondepositorstransactions
 
 import (
-	log "github.com/sirupsen/logrus"
+	"encoding/hex"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/migalabs/eth-pokhar/alchemy"
+	"github.com/migalabs/eth-pokhar/models"
+	logger "github.com/sirupsen/logrus"
 )
 
 func (b *BeaconDepositorsTransactions) getDepositsCheckpoint() uint64 {
 	lastDeposit, err := b.dbClient.ObtainLastDeposit()
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
 	return lastDeposit.BlockNum
+}
+func (b *BeaconDepositorsTransactions) processDepositTransfers(transfers []alchemy.AssetTransfer, numWorkers int) error {
+	var wg sync.WaitGroup
+	errCh := make(chan error, len(transfers))
+	transferCh := make(chan alchemy.AssetTransfer, len(transfers))
+
+	// Create worker pool
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for transfer := range transferCh {
+				// Your existing processing logic here...
+				txHash := common.HexToHash(transfer.Hash)
+				var attempt int
+				for {
+					receipt, err := b.ethClient.TransactionReceipt(b.ctx, txHash)
+					if err != nil {
+						if strings.Contains(err.Error(), "429") {
+							logger.Debugf("Retrying after 429 error (Attempt %d)\n", attempt)
+							time.Sleep(1 * time.Second)
+							attempt++
+							continue
+						}
+						logger.Errorf("Error getting receipt for tx %s: %s", txHash.String(), err.Error())
+						errCh <- err
+						return
+					}
+					if receipt.Status == 1 {
+						for _, log := range receipt.Logs {
+							event := make(map[string]interface{})
+							err := b.contractABI.UnpackIntoMap(event, "DepositEvent", log.Data)
+							if err != nil {
+								logger.Debugf("Error unpacking event: %s", err.Error())
+								continue
+							}
+							pubkey := hex.EncodeToString(event["pubkey"].([]byte))
+							blockNum, err := strconv.ParseUint(strings.TrimPrefix(transfer.BlockNum, "0x"), 16, 64)
+							if err != nil {
+								logger.Errorf("Error parsing block number: %s", err.Error())
+								errCh <- err
+								return
+							}
+							deposit := models.BeaconDeposit{
+								BlockNum:        blockNum,
+								Depositor:       strings.TrimPrefix(transfer.From, "0x"),
+								TxHash:          strings.TrimPrefix(txHash.String(), "0x"),
+								ValidatorPubkey: pubkey,
+							}
+							b.dbClient.Persist(deposit)
+						}
+					}
+					break
+				}
+			}
+		}()
+	}
+
+	// Feed transfers to the worker pool
+	go func() {
+		for _, transfer := range transfers {
+			transferCh <- transfer
+		}
+		close(transferCh)
+	}()
+
+	// Wait for all workers to finish
+	wg.Wait()
+
+	// Close error channel after all workers are done
+	close(errCh)
+
+	// Collect errors from goroutines
+	for err := range errCh {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- beacon-depositors-transactions/routines.go
+++ beacon-depositors-transactions/routines.go
@@ -1,15 +1,12 @@
 package beacondepositorstransactions
 
 import (
-	"encoding/hex"
 	"strconv"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/migalabs/eth-pokhar/alchemy"
-	"github.com/migalabs/eth-pokhar/models"
 	"github.com/migalabs/eth-pokhar/utils"
 )
 
@@ -23,55 +20,32 @@ func (b *BeaconDepositorsTransactions) downloadBeaconDeposits() {
 
 	lastBlocknumProcessed := b.getDepositsCheckpoint()
 	log.Infof("Last block processed: %d", lastBlocknumProcessed)
-	lastBlocknumProcessedHex := "0x" + strconv.FormatUint(lastBlocknumProcessed, 16)
+	startBlock := uint64(0)
+	if lastBlocknumProcessed > 1000 {
+		startBlock = lastBlocknumProcessed - 1000
+	}
+	lastBlocknumProcessedHex := "0x" + strconv.FormatUint(startBlock, 16)
 	params := alchemy.NewGetAssetTransfersArgs(
 		alchemy.SetToAddress(utils.BeaconContractAddress),
 		alchemy.SetFromBlock(lastBlocknumProcessedHex),
+		alchemy.SetCategory([]string{"external", "internal"}),
 	)
 	for params.PageKey != "" || firstCall {
 		newTransfers, newPageKey, err := b.alchemyClient.GetAssetTransfers(b.ctx, params)
 		if err != nil {
 			log.Fatal(err)
 		}
+		num, err := strconv.ParseUint(strings.TrimPrefix(newTransfers[0].BlockNum, "0x"), 16, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Debugf("Downloaded 1000 more deposits on block %d", num)
 		params.PageKey = newPageKey
 		firstCall = false
-		err = b.processTransfers(newTransfers)
+		err = b.processDepositTransfers(newTransfers, 10)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 	}
-}
-
-func (b *BeaconDepositorsTransactions) processTransfers(transfers []alchemy.AssetTransfer) error {
-	for _, transfer := range transfers {
-		txHash := common.HexToHash(transfer.Hash)
-		receipt, err := b.ethClient.TransactionReceipt(b.ctx, txHash)
-		if err != nil {
-			return err
-		}
-		if receipt.Status == 1 {
-			for _, log := range receipt.Logs {
-
-				event := make(map[string]interface{})
-				err := b.contractABI.UnpackIntoMap(event, "DepositEvent", log.Data)
-				if err != nil {
-					return err
-				}
-				pubkey := hex.EncodeToString(event["pubkey"].([]byte))
-				blockNum, err := strconv.ParseUint(strings.TrimPrefix(transfer.BlockNum, "0x"), 16, 64)
-				if err != nil {
-					return err
-				}
-				deposit := models.BeaconDeposit{
-					BlockNum:        blockNum,
-					Depositor:       strings.TrimPrefix(transfer.From, "0x"),
-					TxHash:          strings.TrimPrefix(txHash.String(), "0x"),
-					ValidatorPubkey: pubkey,
-				}
-				b.dbClient.Persist(deposit)
-			}
-		}
-	}
-	return nil
 }
