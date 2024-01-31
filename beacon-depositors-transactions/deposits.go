be03b0c1ae90a712b12b438006c098eebe11269b
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2024-01-31 16:30:17 -0300

    fix bug of random deposits not being stored

--- beacon-depositors-transactions/deposits.go
+++ beacon-depositors-transactions/deposits.go
@@ -21,26 +21,36 @@ func (b *BeaconDepositorsTransactions) getDepositsCheckpoint() uint64 {
 
 	return lastDeposit.BlockNum
 }
+
 func (b *BeaconDepositorsTransactions) processDepositTransfers(transfers []alchemy.AssetTransfer, numWorkers int) error {
 	var wg sync.WaitGroup
+
 	errCh := make(chan error, len(transfers))
 	transferCh := make(chan alchemy.AssetTransfer, len(transfers))
-
+	depositsCh := make(chan models.BeaconDeposit, len(transfers))
 	// Create worker pool
 	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
+			alreadyProcessed := make(map[string]bool)
 			for transfer := range transferCh {
 				// Your existing processing logic here...
 				txHash := common.HexToHash(transfer.Hash)
+				if _, ok := alreadyProcessed[transfer.Hash]; ok {
+					continue
+				}
+				alreadyProcessed[transfer.Hash] = true
 				var attempt int
 				for {
 					receipt, err := b.ethClient.TransactionReceipt(b.ctx, txHash)
+
 					if err != nil {
 						if strings.Contains(err.Error(), "429") {
-							logger.Debugf("Retrying after 429 error (Attempt %d)\n", attempt)
-							time.Sleep(1 * time.Second)
+							if attempt > 10 {
+								logger.Debugf("Retrying after 429 error (Attempt %d)\n", attempt)
+							}
+							time.Sleep(250 * time.Millisecond)
 							attempt++
 							continue
 						}
@@ -53,7 +63,6 @@ func (b *BeaconDepositorsTransactions) processDepositTransfers(transfers []alche
 							event := make(map[string]interface{})
 							err := b.contractABI.UnpackIntoMap(event, "DepositEvent", log.Data)
 							if err != nil {
-								logger.Debugf("Error unpacking event: %s", err.Error())
 								continue
 							}
 							pubkey := hex.EncodeToString(event["pubkey"].([]byte))
@@ -69,7 +78,7 @@ func (b *BeaconDepositorsTransactions) processDepositTransfers(transfers []alche
 								TxHash:          strings.TrimPrefix(txHash.String(), "0x"),
 								ValidatorPubkey: pubkey,
 							}
-							b.dbClient.Persist(deposit)
+							depositsCh <- deposit
 						}
 					}
 					break
@@ -86,18 +95,28 @@ func (b *BeaconDepositorsTransactions) processDepositTransfers(transfers []alche
 		close(transferCh)
 	}()
 
+	go func() {
+		var deposits []models.BeaconDeposit
+		for deposit := range depositsCh {
+			deposits = append(deposits, deposit)
+		}
+		if len(deposits) > 0 {
+			b.dbClient.CopyBeaconDeposits(deposits)
+		}
+	}()
+
 	// Wait for all workers to finish
 	wg.Wait()
 
 	// Close error channel after all workers are done
 	close(errCh)
-
 	// Collect errors from goroutines
 	for err := range errCh {
 		if err != nil {
 			return err
 		}
 	}
+	close(depositsCh)
 
 	return nil
 }
--- db/beacon_deposits.go
+++ db/beacon_deposits.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"time"
+
+	pgx "github.com/jackc/pgx/v5"
 	"github.com/migalabs/eth-pokhar/models"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
@@ -54,4 +57,95 @@ func (p *PostgresDBService) ObtainLastDeposit() (models.BeaconDeposit, error) {
 func BeaconDepositOperation(inputBeaconDeposit models.BeaconDeposit) (string, []interface{}) {
 	q, args := insertBeaconDeposit(inputBeaconDeposit)
 	return q, args
+}
+
+func (p *PostgresDBService) CopyBeaconDeposits(rowSrc []models.BeaconDeposit) int64 {
+
+	startTime := time.Now()
+
+	deposits := beaconDepositToCopyData(rowSrc)
+
+	// Create a temporary table with a unique constraint
+	_, err := p.psqlPool.Exec(p.ctx, `
+        CREATE TEMP TABLE temp_deposits (
+            f_block_num bigint,
+            f_depositor text,
+            f_tx_hash text,
+            f_validator_pubkey text,
+            UNIQUE (f_tx_hash, f_validator_pubkey)
+        )
+    `)
+	if err != nil {
+		wlog.Fatalf("could not create temporary table: %s", err.Error())
+	}
+
+	// Add the trigger function to the temporary table
+	_, err = p.psqlPool.Exec(p.ctx, `
+		CREATE OR REPLACE FUNCTION prevent_duplicates()
+		RETURNS TRIGGER AS $$
+		BEGIN
+			IF NEW.f_tx_hash IS NOT NULL AND NEW.f_validator_pubkey IS NOT NULL THEN
+				IF EXISTS (
+					SELECT 1 FROM temp_deposits
+					WHERE f_tx_hash = NEW.f_tx_hash AND f_validator_pubkey = NEW.f_validator_pubkey
+				) THEN
+					RETURN NULL; -- Ignore duplicate
+				END IF;
+			END IF;
+
+			RETURN NEW;
+		END;
+		$$ LANGUAGE plpgsql;
+
+		CREATE TRIGGER prevent_duplicates_trigger
+		BEFORE INSERT OR UPDATE
+		ON temp_deposits
+		FOR EACH ROW
+		EXECUTE FUNCTION prevent_duplicates();
+	`)
+	if err != nil {
+		wlog.Fatalf("could not add trigger to temporary table: %s", err.Error())
+	}
+
+	// Copy data into the temporary table, ignoring duplicates
+	_, err = p.psqlPool.CopyFrom(
+		p.ctx,
+		pgx.Identifier{"temp_deposits"},
+		[]string{"f_block_num", "f_depositor", "f_tx_hash", "f_validator_pubkey"},
+		pgx.CopyFromRows(deposits),
+	)
+	if err != nil {
+		wlog.Fatalf("could not copy rows into temporary table: %s", err.Error())
+	}
+
+	// Insert non-duplicate rows from the temporary table into the target table
+	count, err := p.psqlPool.Exec(p.ctx, `
+        INSERT INTO t_beacon_deposits (f_block_num, f_depositor, f_tx_hash, f_validator_pubkey)
+        SELECT f_block_num, f_depositor, f_tx_hash, f_validator_pubkey
+        FROM temp_deposits
+        ON CONFLICT DO NOTHING
+    `)
+	if err != nil {
+		wlog.Fatalf("could not insert rows into target table: %s", err.Error())
+	}
+
+	// Drop the temporary table
+	_, err = p.psqlPool.Exec(p.ctx, `DROP TABLE temp_deposits`)
+	if err != nil {
+		wlog.Fatalf("could not drop temporary table: %s", err.Error())
+	}
+
+	wlog.Infof("persisted %d rows in %f seconds", count.RowsAffected(), time.Since(startTime).Seconds())
+
+	return count.RowsAffected()
+}
+
+func beaconDepositToCopyData(rows []models.BeaconDeposit) [][]interface{} {
+	var copyData [][]interface{}
+
+	for _, row := range rows {
+		copyData = append(copyData, []interface{}{row.BlockNum, row.Depositor, row.TxHash, row.ValidatorPubkey})
+	}
+
+	return copyData
 }
