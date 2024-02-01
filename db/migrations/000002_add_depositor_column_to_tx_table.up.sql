e7f80c7700748242e80fa3ceb1762e46700c4dd5
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2024-02-01 12:31:03 -0300

    Add depositor column to Transaction model and table

---
+++ db/migrations/000002_add_depositor_column_to_tx_table.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE t_beacon_depositors_transactions
+ADD IF NOT EXISTS f_depositor text;
--- models/transaction.go
+++ models/transaction.go
@@ -1,11 +1,12 @@
 package models
 
 type Transaction struct {
-	BlockNum uint64  `json:"block_num"`
-	Value    float64 `json:"value"`
-	From     string  `json:"from"`
-	To       string  `json:"to"`
-	TxHash   string  `json:"tx_hash"`
+	BlockNum  uint64  `json:"block_num"`
+	Value     float64 `json:"value"`
+	From      string  `json:"from"`
+	To        string  `json:"to"`
+	TxHash    string  `json:"tx_hash"`
+	Depositor string  `json:"depositor"`
 }
 
 func (t Transaction) Type() ModelType {
