c15fc6ac2a6b6c24b6380251fbf459ba469ce7f2
“santi1234567” <45318759+santi1234567@users.noreply.github.com>
2024-12-18 18:02:17 -0300

    implement lido csm identify routine

--- identify/lido.go
+++ identify/lido.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 
 	db "github.com/migalabs/eth-pokhar/db"
+	"github.com/migalabs/eth-pokhar/lido/csm"
 	"github.com/migalabs/eth-pokhar/lido/curated"
 	log "github.com/sirupsen/logrus"
 )
@@ -13,10 +14,119 @@ import (
 const maxBatchSize = 100
 
 func (i *Identify) IdentifyLidoValidators() error {
+	log.Debug("Identifying lido curated module validators")
 	err := i.identifyCuratedModule()
 	if err != nil {
 		return err
 	}
+	log.Debug("Identified lido curated module validators")
+
+	log.Debug("Identifying lido csm validators")
+	err = i.identifyCSM()
+	if err != nil {
+		return err
+	}
+	log.Debug("Identified lido csm validators")
+
+	return nil
+}
+
+///// CSM /////
+
+func (i *Identify) identifyCSM() error {
+	savedOperatorsValidatorsCount, err := i.dbClient.ObtainLidoOperatorsValidatorCount(db.LidoProtocolCSM)
+	if err != nil {
+		return err
+	}
+
+	log.Debug("Creating a new instance of LidoContract")
+	csmContract, err := csm.NewCSMContract(i.iConfig.ElEndpoint)
+	// Check if there was an error
+	if err != nil {
+		return err
+	}
+
+	log.Debug("Calling the GetOperatorsIndexes function")
+	operatorsCount, err := csmContract.GetNodeOperatorsCount()
+	if err != nil {
+		return err
+	}
+	log.Debugf("Found %v operators", operatorsCount)
+
+	log.Debug("Getting keys for each operator")
+	workerSemaphore := make(chan struct{}, 10)
+	var wg sync.WaitGroup
+
+	for operatorIndex := int64(0); operatorIndex < operatorsCount; operatorIndex++ {
+		if i.stop {
+			break
+		}
+		wg.Add(1)
+		workerSemaphore <- struct{}{}
+		operator, err := csmContract.GetOperatorData(big.NewInt(operatorIndex))
+		if err != nil {
+			return err
+		}
+
+		savedOperatorValidatorCount := uint64(0)
+		if operator.Index < uint64(len(savedOperatorsValidatorsCount)) {
+			savedOperatorValidatorCount = savedOperatorsValidatorsCount[operator.Index]
+		}
+		go func(operator csm.NodeOperatorCustom, savedOperatorValidatorCount uint64) {
+			defer wg.Done()
+			i.processCSMOperatorKeys(operator, savedOperatorValidatorCount)
+			<-workerSemaphore
+		}(operator, savedOperatorValidatorCount)
+	}
+	log.Debug("Finished getting keys for each operator")
+
+	wg.Wait()
+	if i.stop {
+		return nil
+	}
+
+	log.Debug("Identifying lido curated validators")
+	err = i.dbClient.IdentifyLidoValidators()
+	if err != nil {
+		return err
+	}
+	log.Debug("Identified lido validators")
+
+	return nil
+}
+
+func (i *Identify) processCSMOperatorKeys(operator csm.NodeOperatorCustom, savedOperatorValidatorCount uint64) error {
+	savedOperatorValidatorCount32 := uint32(savedOperatorValidatorCount)
+
+	log.Debug("Creating a new instance of LidoContract")
+	lidoContract, err := csm.NewCSMContract(i.iConfig.ElEndpoint)
+	// Check if there was an error
+	if err != nil {
+		return err
+	}
+	log.Debug("Created a new instance of LidoContract")
+
+	operatorName := csm.GetOperatorName(operator)
+	log.Infof("Getting new keys for operator %v", operatorName)
+	validKeysCount := operator.Operator.TotalAddedKeys - operator.Operator.EnqueuedCount
+
+	if validKeysCount-savedOperatorValidatorCount32 == 0 {
+		log.Infof("No new keys for operator %v", operatorName)
+		return nil
+	}
+	remainingKeys := validKeysCount - savedOperatorValidatorCount32
+
+	keys, err := lidoContract.GetOperatorKeys(operator, uint64(savedOperatorValidatorCount32), uint64(remainingKeys))
+	if err != nil {
+		return err
+	}
+	keysString := make([]string, len(keys))
+	for i, key := range keys {
+		keysString[i] = hex.EncodeToString(key)
+	}
+	count := i.dbClient.CopyLidoOperatorValidators(operatorName, operator.Index, keysString, db.LidoProtocolCSM)
+	log.Debugf("Inserted %v validators for operator %v", count, operatorName)
+	log.Infof("Got %v new keys for operator %v", remainingKeys, operatorName)
 
 	return nil
 }
@@ -67,7 +177,7 @@ func (i *Identify) identifyCuratedModule() error {
 		}
 		go func(operator curated.NodeOperator, operatorValidatorCount uint64) {
 			defer wg.Done()
-			i.processOperatorKeys(operator, operatorValidatorCount)
+			i.processCuratedOperatorKeys(operator, operatorValidatorCount)
 			<-workerSemaphore
 		}(operator, operatorValidatorCount)
 	}
@@ -88,7 +198,7 @@ func (i *Identify) identifyCuratedModule() error {
 	return nil
 }
 
-func (i *Identify) processOperatorKeys(operator curated.NodeOperator, operatorValidatorCount uint64) error {
+func (i *Identify) processCuratedOperatorKeys(operator curated.NodeOperator, operatorValidatorCount uint64) error {
 	log.Debug("Creating a new instance of LidoContract")
 	lidoContract, err := curated.NewCuratedModuleContract(i.iConfig.ElEndpoint)
 	// Check if there was an error
