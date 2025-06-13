bdf5633a90b8a7fca6a45d74cb5e1e94472d4a43
“santi1234567” <45318759+santi1234567@users.noreply.github.com>
2025-06-13 10:32:09 -0300

    fix: update SQL query in ApplyDepositorsInsert method for using last depositor address for tagging

--- db/depositors_insert.go
+++ db/depositors_insert.go
@@ -4,20 +4,28 @@ import "github.com/pkg/errors"
 
 const (
 	applyDepositorsInsertQuery = `
-	INSERT INTO t_identified_validators (
-		f_validator_pubkey,
-		f_pool_name
-	)
-	SELECT DISTINCT 
-		t1.f_validator_pubkey, 
-		f_pool_name 
-	FROM 
-		t_beacon_deposits t1
-	RIGHT JOIN 
-		t_depositors_insert t2 
-	ON 
-		t1.f_depositor = t2.f_depositor
-	ON CONFLICT (f_validator_pubkey) DO UPDATE SET f_pool_name = EXCLUDED.f_pool_name;
+		INSERT INTO t_identified_validators (
+			f_validator_pubkey,
+			f_pool_name
+		)
+		SELECT DISTINCT 
+			t1.f_validator_pubkey, 
+			t2.f_pool_name 
+		FROM (
+			SELECT 
+				f_validator_pubkey,
+				f_depositor,
+				ROW_NUMBER() OVER (
+					PARTITION BY f_validator_pubkey 
+					ORDER BY f_block_num DESC
+				) as rn
+			FROM t_beacon_deposits
+		) t1
+		INNER JOIN t_depositors_insert t2 
+			ON t1.f_depositor = t2.f_depositor
+		WHERE t1.rn = 1
+		ON CONFLICT (f_validator_pubkey) DO UPDATE SET 
+			f_pool_name = EXCLUDED.f_pool_name;
 	`
 )
 
