67d4e5ae113688672cf602329d7449906e9b350d
Zyra-V21 <zyrav21@proton.me>
2026-07-25 12:16:36 +0200

    perf(db): read latest deposit facts from t_validator_last_deposit instead of window scans

--- db/depositors_insert.go
+++ db/depositors_insert.go
@@ -8,22 +8,12 @@ const (
 			f_validator_pubkey,
 			f_pool_name
 		)
-		SELECT DISTINCT 
-			t1.f_validator_pubkey, 
-			t2.f_pool_name 
-		FROM (
-			SELECT 
-				f_validator_pubkey,
-				f_depositor,
-				ROW_NUMBER() OVER (
-					PARTITION BY f_validator_pubkey 
-					ORDER BY f_block_num DESC
-				) as rn
-			FROM t_beacon_deposits
-		) t1
-		INNER JOIN t_depositors_insert t2 
-			ON t1.f_depositor = t2.f_depositor
-		WHERE t1.rn = 1
+		SELECT
+			v.f_validator_pubkey,
+			m.f_pool_name
+		FROM t_validator_last_deposit v
+		INNER JOIN t_depositors_insert m
+			ON v.f_depositor = m.f_depositor
 		ON CONFLICT (f_validator_pubkey) DO UPDATE SET
 			f_pool_name = EXCLUDED.f_pool_name
 		WHERE t_identified_validators.f_pool_name IS DISTINCT FROM EXCLUDED.f_pool_name;
